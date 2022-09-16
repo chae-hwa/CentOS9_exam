1cb7757e70089240beb966373bc7145f52943e0d
chae-hwa <ihj0519@naver.com>
2022-09-16 09:37:23 +0900

    세팅

---
+++ src/main/java/com/example/centos9_exam/CentOs9ExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.centos9_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentOs9ExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CentOs9ExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/centos9_exam/CentOs9ExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.centos9_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentOs9ExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
